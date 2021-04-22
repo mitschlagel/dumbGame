e1dfd948dbb5f2a53547b1da098d519fcaccf8ca
Jones <346352@fnni.com>
2021-04-22 15:53:56 -0500

    Attempts at loser/winner pop ups

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 import { randomInt, randomColor, numArray } from "./helpers.js";
-
-// If user guess number of tries required, they win.
-
+// import loserContent from "./GameOver.js";
 
 
-// Random number 1-28 is generated as the title on page load.
 const numTarget = document.querySelector(".num__target");
 numTarget.innerText = randomInt();
 
-// User guesses how many tries it will take to find the number
-// Guess is displayed on page
+
 const guess = document.querySelector("#guess");
 const button = document.querySelector("#guess__button");
 const displayGuess = document.querySelector("#you__guessed");
@@ -17,36 +13,58 @@ const clicksRemaining = document.querySelector("#clicks__remaining");
 button.addEventListener(
     "click",
     () => {
+
+        // Start game, populate divs with user guess
         console.log(guess.value);
         displayGuess.innerText = guess.value;
         clicksRemaining.innerText = guess.value;
+
+
+
+        const cellArray = document.querySelectorAll(".cell");
+        const nums = numArray(); // Create int array 1-28 in random order
+        const isSetArray = [];  // Array to store cell state
+
+        // Populate cell with random num and color when clicked
+        cellArray.forEach((cell) => {
+            
+            
+            cell.addEventListener("click",
+                (e) => {
+                    console.log(displayGuess.value);
+
+                        // Check if num has alreay been assigned
+                        if(!isSetArray[e.target.className.substr(-2)]) {
+                            
+                            cell.style.backgroundColor = randomColor();
+                            let num = nums.pop();
+                            cell.innerHTML = num;  // Populate each cell from int array
+                            isSetArray[e.target.className.substr(-2)] = num;
+                            clicksRemaining.innerText--;
+                            
+                            if (clicksRemaining.innerText == 0) {
+                                
+                                const loser = document.querySelector(".loser");
+                                loser.style.display = "block";
+                                
+                                console.log("YOU LOSE");
+                            } else if (cell.innerText === numTarget.innerText) {
+                                console.log("YOU WIN");
+                            }
+                        }
+
+                        console.log(nums.length);
+                    
+                    },
+                    false
+                
+            );
+                    
+        });
     },
     false
 );
 
 // Numbers randomly assigned to each box
-const cellArray = document.querySelectorAll(".cell");
-const nums = numArray(); // Create int array 1-28 in random order
 
-cellArray.forEach((cell) => {
-    cell.addEventListener("click",
-        (e) => {
-            cell.style.backgroundColor = randomColor();
-            if (nums.length > 0) {
-                cell.innerHTML = nums.pop();  // Populate each cell from int array
-                clicksRemaining.innerText--;
-                if (clicksRemaining.innerText == 0) {
-                    console.log("YOU LOSE");
-                } else if (cell.innerText === numTarget.innerText) {
-                    console.log("YOU WIN");
-                }
-            }
 
-            console.log(nums.length);
-        },
-        false
-    );
-
-});
-
-//TO DO: stop the event listener when num array is empty
